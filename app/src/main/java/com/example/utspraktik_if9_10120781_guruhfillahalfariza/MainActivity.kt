4922e7e092e95ff396fea23acc93e992cd8cb282
TakaAlfariza <gfilah86@gmail.com>
2023-05-25 10:36:01 +0700

    first commit

---
+++ app/src/main/java/com/example/utspraktik_if9_10120781_guruhfillahalfariza/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.utspraktik_if9_10120781_guruhfillahalfariza
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
